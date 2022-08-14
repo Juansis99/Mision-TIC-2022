83fdf1c667d5517c09dec33724608d315f3372dd
Juansis99 <jsimoncardona@hotmail.com>
2022-08-14 15:47:41 -0500

    Create Project - Getters and Setters.

---
+++ Ciclo_3_Web/Tutorial_Java_SpringBoot/demo/src/main/java/com/example/demo/Task.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+
+public class Task {
+    //Atributos
+    private String description;
+    private boolean done;
+    private LocalDate duedate;
+
+    //Constructores
+    public Task(String description, boolean done, LocalDate duedate) {
+        this.description = description;
+        this.done = done;
+        this.duedate = duedate;
+    }
+
+    //Getters y Setters
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public LocalDate getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(LocalDate duedate) {
+        this.duedate = duedate;
+    }
+}
